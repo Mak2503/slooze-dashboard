abdbad2ebf7c59803d5b0a8c436fa22e522328e7
Mohamed Abdul Kader <mak2503sde@gmail.com>
2025-03-20 17:28:37 +0530

    comment unused props

--- src/components/ShipmentAnalytics/TrackShipment.tsx
+++ src/components/ShipmentAnalytics/TrackShipment.tsx
@@ -6,17 +6,17 @@ interface TrackShipmentProps {
   shipmentId: string;
   from: string;
   to: string;
-  progress?: number; // to be done at last
+  // progress?: number; // to be done at last
 }
 
 const TrackShipment: React.FC<TrackShipmentProps> = ({
   shipmentId,
   from,
   to,
-  progress,
+  // progress,
 }) => {
   return (
-    <div className='bg-primaryBg py-3 pl-4 rounded-lg'>
+    <div className='bg-primaryBg rounded-lg pl-4 py-3'>
       <div className='flex justify-between'>
         <div className='space-y-2'>
           <p className='text-xxs opacity-50'>Shipment ID</p>
@@ -24,13 +24,13 @@ const TrackShipment: React.FC<TrackShipmentProps> = ({
         </div>
         <Image src='/truck-with-progress.svg' alt='truck' width={110} height={55} />
       </div>
-      <div className='flex items-center gap-2 text-xxs'>
-        <div className='flex items-center gap-2'>
+      <div className='flex text-xxs gap-2 items-center'>
+        <div className='flex gap-2 items-center'>
           <Image src="/from-point.svg" alt='from-point' width={12} height={12} />
           <p>{from}</p>
         </div>
         <MoveRight />
-        <div className='flex items-center gap-2'>
+        <div className='flex gap-2 items-center'>
           <Image src="/to-point.svg" alt='to-point' width={12} height={12} />
           <p>{to}</p>
         </div>
